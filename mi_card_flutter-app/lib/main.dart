f9d311b829a6ccedcee838afec807a379b9a6aac
Eduardo Velez <evelez1990@hotmail.com>
2020-06-12 22:05:34 -0400

    Added StatelessWidget

---
+++ mi_card_flutter-app/lib/main.dart
@@ -0,0 +1,18 @@
+//Eduardo Velez
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(child: Container()),
+      ),
+    );
+  }
+}
