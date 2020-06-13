c4e4c190cbb11bb4fd7d117279b6326bc48f333e
Eduardo Velez <evelez1990@hotmail.com>
2020-06-12 20:40:19 -0400

    initial commit

--- i_am_poor_app/lib/main.dart
+++ i_am_poor_app/lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.brown,
+      appBar: AppBar(
+        title: Text('I am Poor'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    ),
+  ));
 }
-
